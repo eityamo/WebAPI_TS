8f4f2e5f513ec2018f3dd3b20fa7127598db4614
eityamo <eityamo@gmail.com>
2022-09-19 11:58:04 +0900

    add:githubAPIを叩く

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,97 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue'
+import axios from 'axios'
+
+type User = {
+  name: string
+  login: string
+  location: string
+  publicRepos: number
+  avatarUrl: string
+  htmlUrl: string
+}
+
+type Response = {
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
+  name: string
+  company: string
+  blog: string
+  location: string
+  email: string
+  hireable: string
+  bio: string
+  twitter_username: string
+  public_repos: number
+  public_gists: number
+  followers: number
+  following: number
+  created_at: string
+  updated_at: string
+}
+
+// リアクティブな変数 プリミティブref オブジェクトreactive
+// データプロパティ userInfo
+const userInfo = reactive<User>({
+  name: '',
+  login: '',
+  location: '',
+  publicRepos: 0,
+  avatarUrl: '',
+  htmlUrl: ''
+})
+const userId = 'eityamo'
+
+const fetchUserInfo = (): void => {
+  axios.get<Response>(`https://api.github.com/users/${userId}`)
+  .then(( response ) => {
+    const {name, login, location, public_repos, avatar_url, html_url} = response.data
+    userInfo.name = name
+    userInfo.login = login
+    userInfo.location = location
+    userInfo.publicRepos = public_repos
+    userInfo.avatarUrl = avatar_url
+    userInfo.htmlUrl = html_url
+  })
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h2>GitHub User Info</h2>
+  <button @click="fetchUserInfo">Get user info</button>
+  <!-- 整形したHTMLの挿入先 -->
+  <div id="result">
+    <h4>{{ userInfo.name }} @{{ userInfo.login }}</h4>
+    <img :src="userInfo.avatarUrl" :alt="userInfo.login" height="100">
+    <dl>
+      <dt>Location</dt>
+      <dd>{{ userInfo.location }}</dd>
+
+      <dt>Repositories</dt>
+      <dd>{{ userInfo.publicRepos }}</dd>
+
+      <dt>Github_Link</dt>
+      <dd>{{ userInfo.htmlUrl }}</dd>
+    </dl>
   </div>
-  <HelloWorld msg="button" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
